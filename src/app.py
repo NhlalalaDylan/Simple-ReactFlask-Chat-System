57054c263fb03473979b05aef0718463d92746db
NhlalalaDylan <Nhlalalamaluleke332@gmail.com>
2021-03-31 20:08:55 +0200

    chat system

---
+++ src/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify;
+from flask_socketio import SocketIO, send
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecret'
+
+socketIo = SocketIO(app, cors_allowed_origins="*")
+
+app.debug = True
+app.host = 'localhost'
+
+@app.route("/")
+def index():
+    return "Hello"
+
+@socketIo.on("message")
+def handleMessage(msg):
+    print(msg)
+    send(msg, broadcast=True)
+    return None
+
+if __name__ == '__main__':
+    socketIo.run(app)
